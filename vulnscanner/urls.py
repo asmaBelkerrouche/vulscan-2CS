7fd79d33385635f12299e174ec5b81c431ae6994
asmaBelkerrouche <164345614+asmaBelkerrouche@users.noreply.github.com>
2025-11-30 00:12:19 +0100

    Add home view for root URL in vulnscanner

--- vulnscanner/urls.py
+++ vulnscanner/urls.py
@@ -8,15 +8,18 @@ to appropriate application-specific URL configurations.
 The `urlpatterns` list routes URLs to views. For more information please see:
 https://docs.djangoproject.com/en/5.2/topics/http/urls/
 """
-
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+def home(request):
+    return HttpResponse("Backend is running!")
+
 # Main URL patterns for the VulnScan application
 urlpatterns = [
-    path('', home),  # <-- add this
+    path('', home),  # Root URL now shows a message
     # Django administration interface
     path('admin/', admin.site.urls),
 
